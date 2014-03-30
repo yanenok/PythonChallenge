c22ce28bf37d230e10378e638c28a8ce247fdd0d
Yanenok <yanenok@server.fake>
2014-03-30 23:49:14 +0400

    Added solution to Level 4

---
+++ Level4-1.py
@@ -0,0 +1,20 @@
+import urllib
+import re
+
+link = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
+num = 12345
+
+url = urllib.urlopen(link + str(num))
+i = 0
+
+while (i < 400):
+    text = url.readlines()
+    print text
+    found = text[0].find('next nothing is ')
+    if found > 0:
+        num = int(text[0][found+16:])
+        print num
+        url = urllib.urlopen(link + str(num))
+        i = i + 1
+    else:
+        break
--- README.md
+++ README.md
@@ -15,3 +15,6 @@ Using OrderedDict to get keys in order they appear.
 
 ## Level 3
 Looking for strictly 3 upperCase characters to the left and to the right of 1 lowerCase symbol.
+
+## Level 4
+Looking for a number after "and the next nothing is ". The loop prints each message and stops when a different text is found. Parts 4-2 and 4-3 start off with the numbers from those instructions. 
